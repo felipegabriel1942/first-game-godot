4ecdd14111d18995022b230c172dbac26486dadd
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2024-04-30 12:20:34 -0300

    criando label para score

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,6 +2,20 @@ extends Node
 
 var score = 0
 
+@onready var score_label = %ScoreLabel
+
 func add_point():
 	score += 1
-	print(score)
+	_update_score_label()
+
+func _update_score_label():
+	var prefix = '';
+	
+	if score >= 0 :
+		prefix = '00'
+	if score >= 10:
+		prefix = '0'
+		
+	score_label.text = 'x' + prefix + str(score)
+	
+
